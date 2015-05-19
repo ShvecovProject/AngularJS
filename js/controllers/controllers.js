3d9e6a14fe5323b2b1078d5c46030681562f4c18
Evgeniy Shvecov <evgeshvecov@yandex.ru>
2015-05-19 18:34:11 +0300

    commit RESFULL

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -9,10 +9,15 @@ var notificationArhive = function(){
     }
 };
 
-angular.module('helloApp', ['ui.keypress'])
+angular.module('helloApp', ['ui.keypress','ngResource'])
+
 .service('notificationArhive',notificationArhive )
+
 .config(function(notificationsServiceProvider){
     notificationsServiceProvider.setMaxLength(15);
+})
+.config(function(resourceServiceProvider){
+        resourceServiceProvider.setPathToService( "https://api.mongolab.com/api/1/databases/angular_js/collections/users/:id");
 })
 
 .controller('WorldController', function($scope){
@@ -25,6 +30,7 @@ angular.module('helloApp', ['ui.keypress'])
         return (countryPopulation/$scope.population)*100;
     }
 })
+
 .controller("MessageController",function($scope, notificationsService){
     $scope.getCurrentNotif = function(){
         return $scope.notification;
@@ -37,27 +43,36 @@ angular.module('helloApp', ['ui.keypress'])
          }
     }
 })
+
 .controller('HelloController',function($scope){
     $scope.getName = function(){
         return $scope.name;
     };
 })
 
-.controller("HttpFunctionsController",function($scope, $http){
+.controller("HttpFunctionsController",function($scope, $http, resourceService){
         $scope.name = "World";
     var getAllUsers = function() {
-        var response = $http.get(httpUrl, {
+        $scope.users = resourceService.query({},function(){},function(){
+            console.log("Error");
+        });
+        /*var response = $http.get(httpUrl, {
             params: {
                 apiKey: "H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX"
             }
         });
-        response.success(function (data, status, headers, config) {
+        response.then(function(response){
+            $scope.users = response.data;
+        },function(response){
+
+        });*/
+        /*response.success(function (data, status, headers, config) {
             console.log("Users has been getting " + data);
             $scope.users = data;
         });
         response.error(function () {
 
-        });
+        });*/
     };
 
     var httpUrl = "https://api.mongolab.com/api/1/databases/angular_js/collections/users";
@@ -70,24 +85,28 @@ angular.module('helloApp', ['ui.keypress'])
             userEmail: $scope.userEmail || 'default@yandex.ru'
         };
         console.log(JSON.stringify(newUser));
-        var response = $http.post(httpUrl, newUser, {
-            params:{
-                apiKey:"H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX"
-            }
-        });
-        response.success(function(data, status, headers, config){
-            console.log("User has been added "+ data);
-        });
-        response.error(function(){
+        /* var response = $http.post(httpUrl, newUser, {
+         params:{
+         apiKey:"H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX"
+         }
+         });
+         response.success(function(data, status, headers, config){
+         console.log("User has been added "+ data);
+         });
+         response.error(function(){
 
+         });*/
+        resourceService.save({}, newUser, function(data){
+            $scope.users.push(data);
+        }, function () {
+            console.log("Error");
         });
-
         $scope.userEmail = "";
         $scope.userName = "";
-        getAllUsers();
     };
-    $scope.deleteUser=function(idUser) {
-        var response = $http.delete(httpUrl,idUser,{
+
+    $scope.deleteUser=function(user) {
+       /* var response = $http.delete(httpUrl,idUser,{
             params: {
                 apiKey: "H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX"
             }
@@ -97,9 +116,29 @@ angular.module('helloApp', ['ui.keypress'])
         });
         response.error(function () {
 
+        });*/
+      /*  $http.delete(httpUrl+"/"+idUser,{
+            params:{
+                apiKey:"H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX"
+            }
+        });*/
+
+        /*$scope.delete = function ( idx ) {
+         var person_to_delete = $scope.persons[idx];
+
+         API.DeletePerson({ id: person_to_delete.id }, function (success) {
+         $scope.persons.splice(idx, 1);
+         });
+         };*/
+        resourceService.delete({},user, function(){
+                $scope.users.splice(user,1);
+            },
+         function(){
+            console.log("Error");
         });
-        getAllUsers();
+
     };
+
     $scope.SendJsonp = function(){
         $http.jsonp( "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts?callback=JSON_CALLBACK",{ //'http://cs621417.vk.me/v621417103/c5f2/VU_ynS7wQL4.jpg?callback=JSON_CALLBACK',{ // 'http://angularjs.org/greet.php?callback=JSON_CALLBACK',{
             params:{
@@ -112,6 +151,20 @@ angular.module('helloApp', ['ui.keypress'])
             });
     };
 })
+
+.controller("ResourceController",function($scope, resourceService){
+    $scope.addUserREST=function(){
+        resourceService.query({},function(data){
+            console.log(data);
+        });
+        console.log("Hello!");
+    };
+    /*$scope.users = Users.query({}, function(users){
+        console.log($scope.users.length);
+    });*/
+
+    })
+
 .provider('notificationsService',function(){
     var config = {
         maxLen:10
@@ -139,7 +192,37 @@ angular.module('helloApp', ['ui.keypress'])
             }
         }
     }
+})
+
+.factory('Users', function($resource){
+        var Users =  $resource(
+        "https://api.mongolab.com/api/1/databases/angular_js/collections/users/:id",
+        {
+            apiKey:"H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX",
+            id:'@_id.$oid'
+        });
+        return Users;
+})
+
+.provider('resourceService',function(){
+    var config={
+        pathToService: "https://api.mongolab.com/api/1/databases/angular_js/collections/users/:id"
+    };
+    return{
+        setPathToService:function(path){
+            config.pathToService = path||config.pathToService;
+        },
+        $get:function($resource){
+            return $resource (
+                config.pathToService,{
+                    apiKey:"H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX",
+                    id:'@_id.$oid'
+                }
+            )
+        }
+    }
 });
+
 
 
 
